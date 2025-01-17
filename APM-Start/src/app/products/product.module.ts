56c42785c78f4b919ddee10b5d7b43278a27e8c8
James House <james@volanterp.com>
2025-01-16 17:03:55 -0800

    organizing modules

--- APM-Start/src/app/products/product.module.ts
+++ APM-Start/src/app/products/product.module.ts
@@ -3,17 +3,16 @@ import { CommonModule } from '@angular/common';
 import { ProductListComponent } from './product-list.component';
 import { ProductDetailComponent } from './product-detail.component';
 import { ConvertToSpacesPipe } from '../shared/convert-to-spaces.pipe';
-import { StarComponent } from '../shared/star.component';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { ProductDetailGuard } from './product-detail.guard';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
     ProductListComponent,
     ProductDetailComponent,
     ConvertToSpacesPipe,
-    StarComponent
   ],
   imports: [
     CommonModule,
@@ -25,7 +24,8 @@ import { ProductDetailGuard } from './product-detail.guard';
               canActivate: [ProductDetailGuard],
               component: ProductDetailComponent 
             },
-    ])
+    ]),
+    SharedModule
   ]
 })
 export class ProductModule { }
